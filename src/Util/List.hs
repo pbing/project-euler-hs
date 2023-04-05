8663670337127a945d509b3a120173f7cba0facf
Bernd Beuster <bernd.beuster@gmail.com>
2023-04-05 19:02:45 +0200

    Simplify primes

--- src/Util/List.hs
+++ src/Util/List.hs
@@ -5,8 +5,3 @@ module Util.List where
 isSingleton :: [a] -> Bool
 isSingleton [_] = True
 isSingleton _   = False
-
-merge :: [a] -> [a] -> [a]
-merge xs [] = xs
-merge [] ys = ys
-merge (x:xs) (y:ys) = x : y : merge xs ys
--- src/Util/Primes.hs
+++ src/Util/Primes.hs
@@ -1,5 +1,6 @@
 -- Primes and prime factors
 -- https://wiki.haskell.org/Euler_problems/1_to_10
+{-# LANGUAGE ParallelListComp #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Util.Primes where
@@ -11,7 +12,7 @@ primes = 2 : 3: filter (isSingleton . primeFactors) seq6k
   where
     -- (6k - 1), (6k + 1) sequence
     seq6k :: [Int]
-    seq6k = merge [5, 11..] [7, 13..]
+    seq6k = concat [[x, y] | x <- [5, 11..] | y <- [7, 13..]]
 
 primeFactors :: Int -> [Int]
 primeFactors n = factor n primes
