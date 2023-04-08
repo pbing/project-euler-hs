8dfde9c2fb723a1d730ca7cfc0488acd88f9cfe2
Bernd Beuster <bernd.beuster@gmail.com>
2023-04-08 12:44:47 +0200

    Optimize problem092

--- src/Problem092.hs
+++ src/Problem092.hs
@@ -2,13 +2,14 @@
 
 module Problem092 (problem092) where
 
-import Util.Number
-
 problem092 :: Int -> Int
 problem092 n = length $ filter (== 89) $ map terminator [1 .. n]
 
 step :: Int -> Int
-step = foldr (\n -> (+) (n * n)) 0 . digits
+step 0 = 0
+step n = r * r + step q
+  where
+    (q, r) = quotRem n 10
 
 terminator :: Int -> Int
 terminator n
