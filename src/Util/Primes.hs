9aae496a1446098303eda4266f989fcdc1059a24
Bernd Beuster <bernd.beuster@gmail.com>
2024-08-10 15:27:43 +0200

    Optimize division

--- src/Util/Primes.hs
+++ src/Util/Primes.hs
@@ -10,14 +10,14 @@ primes :: (Integral a) => [a]
 primes = 2 : 3 : filter (isSingleton . primeFactors) seq6k
   where
     -- (6k - 1), (6k + 1) sequence
-    seq6k :: (Enum a, Num a) => [a]
-    seq6k = [x + y | x <- [6, 12..], y <- [-1, 1]]
+    seq6k = [x + y | x <- [6, 12 ..], y <- [-1, 1]]
 
 primeFactors :: (Integral a) => a -> [a]
 primeFactors n = factor n primes
   where
-    factor :: Integral a => a -> [a] -> [a]
-    factor m (p:ps)
-        | p * p > m      = [m]
-        | m `mod` p == 0 = p : factor (m `div` p) (p:ps)
-        | otherwise      = factor m ps
+    factor m (p : ps)
+      | p * p > m  = [m]
+      | r == 0     = p : factor q (p : ps)
+      | otherwise  = factor m ps
+      where
+        (q, r) = quotRem m p
