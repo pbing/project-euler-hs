6d7638f6f6cbb870aaefeb4982fc390195e226c9
Bernd Beuster <bernd.beuster@gmail.com>
2024-08-10 15:26:50 +0200

    Uss O(1) algorithm

--- src/Problem001.hs
+++ src/Problem001.hs
@@ -1,7 +1,16 @@
 module Problem001 (problem001) where
 
-problem001 :: Int -> Int
-problem001 n = sum $ filter p [1..(n - 1)]
+-- -- runtime O(n)
+-- problem001 :: Int -> Int
+-- problem001 n = sum $ filter p [1..(n - 1)]
+--
+-- p :: Int -> Bool
+-- p n = n `mod` 3 == 0 || n `mod` 5 == 0
 
-p :: Int -> Bool
-p n = n `mod` 3 == 0 || n `mod` 5 == 0
+-- runtime O(1)
+problem001 :: Int -> Int
+problem001 n = summate 3 + summate 5 - summate (lcm 3 5)
+  where
+    summate a = a * m * (m + 1) `div` 2
+      where
+        m = (n - 1) `div` a
